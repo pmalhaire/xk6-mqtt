d5ee1d0aaed5ea9f523782a1beb5aa233ee41aaf
M. Jaunius Kapkan <38353835+mjkapkan@users.noreply.github.com>
2021-07-28 11:55:59 +0300

    Added option for conversion to binary

--- publish.go
+++ publish.go
@@ -3,11 +3,27 @@ package mqtt
 import (
 	"context"
 	"time"
+	"encoding/hex"
 
 	paho "github.com/eclipse/paho.mqtt.golang"
 	"go.k6.io/k6/js/common"
 	"go.k6.io/k6/lib"
 )
+
+
+func asHex(
+	stringVal string,
+	convertToBin bool,
+) interface{} {
+	if convertToBin == true {
+		data, err := hex.DecodeString(stringVal)
+			if err != nil {
+				panic(err)
+			}
+			return data
+	}
+	return stringVal
+}
 
 // Publish allow to publish one message
 func (*Mqtt) Publish(
@@ -15,9 +31,10 @@ func (*Mqtt) Publish(
 	client paho.Client,
 	topic string,
 	qos int,
-	message byte,
+	message string,
 	retain bool,
 	timeout int,
+	hex2bin bool,
 ) {
 	state := lib.GetState(ctx)
 	if state == nil {
@@ -28,8 +45,8 @@ func (*Mqtt) Publish(
 		common.Throw(common.GetRuntime(ctx), ErrorClient)
 		return
 	}
-
-	token := client.Publish(topic, byte(qos), retain, message)
+	outboundMessage := asHex(message, hex2bin)
+	token := client.Publish(topic, byte(qos), retain, outboundMessage)
 	if !token.WaitTimeout(time.Duration(timeout) * time.Millisecond) {
 		common.Throw(common.GetRuntime(ctx), ErrorTimeout)
 		return
