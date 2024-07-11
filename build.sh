8e429579b59732c374ab7e2c0a908b1548c796cb
Elmar Zeeb <elmar.zeeb@vreeda.com>
2024-07-11 22:28:54 +0200

    removed build file

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-xk6 build v0.52.0 \
-  --replace google.golang.org/genproto@v0.0.0-20210226172003-ab064af71705=google.golang.org/genproto@v0.0.0-20230526161137-0005af68ea54 \
-  --replace google.golang.org/genproto@v0.0.0-20230410155749-daa745c078e1=google.golang.org/genproto@v0.0.0-20230526161137-0005af68ea54 \
-  --with github.com/avitalique/xk6-file@latest \
-  --with github.com/ezeeb/xk6-mqtt=.
